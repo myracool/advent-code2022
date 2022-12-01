fc42acc300e182d7a4d5a58343f5f05105e7184f
mary <mary.emilie@outlook.fr>
2022-12-01 13:00:48 +0100

    day1 - part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::fs::File;
 use std::io::*;
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -10,22 +9,31 @@ fn main() {
 
     let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
+
     let mut sum = 0;
     let mut most = 0;
+    let mut array: [i32; 3] = [0; 3];
+    let mut min_index: usize = 0;
 
-    // Step 2: loop over lines and print them.
     for line in reader.lines() {
         if line.as_ref().unwrap() == "" {
-            if most < sum {
-                most = sum;
+            for (i, _) in array.into_iter().enumerate() {
+                if array[i] < array[min_index] {
+                    min_index = i;
+                }
+            }
+            if sum > array[min_index] {
+                array[min_index] = sum;
             }
             sum = 0;
-        }
-        else {
-            let to_int : i32 =  line.as_ref().expect("omg le rust c'est penible en fait").parse::<i32>().unwrap();
+        } else {
+            let to_int: i32 = line.as_ref().expect("omg le rust c'est penible en fait").parse::<i32>().unwrap();
             sum += to_int;
         }
     }
-
-    println!("MOST: {}", most);
+    for (_, x) in array.iter().enumerate() {
+        most += x;
+    }
+    println!("total: {}", most);
 }
+
